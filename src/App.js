27b0d3586de8c925606240cdab9850669ea29db2
Cloudy Lopez <cloudylopez@gmail.com>
2019-06-11 15:33:28 -0700

    starting project. a bit broken but its being worked on.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import Chatlog from './components/Chatlog';
 
 const App = () => {
   console.log(chatMessages);
@@ -8,10 +9,10 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">WeBeChattin</h1>
       </header>
       <main className="App-main">
-
+      <Chatlog messages={ chatMessages } />
       </main>
     </div>
   );
---
+++ src/components/Messages.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Timestamp from './Timestamp';
+
+
+
+
+
+export default Messages;
